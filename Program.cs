c690bfa7a89d1befd7301c66104c05a265a256a5
Alexandra <alexadudki11@gmail.com>
2023-06-05 14:41:42 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void CreateArray(double[,] array){
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.NextDouble();
+        }
+    }
+}
+void PrintArray(double[,] array){
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int PrintMes(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = PrintMes("Введите количество строк:");
+int n = PrintMes("Введиет количество столбцов:");
+double[,] matrix = new double[m, n];
+CreateArray(matrix);
+PrintArray(matrix);
+
+
